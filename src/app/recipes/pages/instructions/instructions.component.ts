b14c7d5c5487f9f55defb1248bf886b20f3f6f27
Samuel <samuellawrencereddick@gmail.com>
2019-10-16 23:05:23 -0400

    Got embed to work

--- src/app/recipes/pages/instructions/instructions.component.ts
+++ src/app/recipes/pages/instructions/instructions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-instructions',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./instructions.component.scss']
 })
 export class InstructionsComponent implements OnInit {
+  
+  recipeUrl: SafeResourceUrl;
 
-  constructor() { }
+  constructor (public sanitizer:DomSanitizer) {
+
+  }
 
   ngOnInit() {
+    this.recipeUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.google.com');
   }
 
 }
