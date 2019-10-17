94d9f8ac40f0413b9bd0774d0b0bc7d28b5a1b37
LukasGuptaLeary <lukool95@yahoo.com>
2019-10-16 23:32:05 -0400

    getRecipe()

--- src/app/shared/recipe.service.ts
+++ src/app/shared/recipe.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { RecipeSearchModel } from './models/recipe-search.model';
 import { tap } from 'rxjs/operators';
+import {RecipeModel} from './models/recipe.model';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,9 @@ export class RecipeService {
   recipeSearch(q: string) {
     const params = new HttpParams().set('q', q);
     return this.http.get<RecipeSearchModel>('https://api-dot-mimir-cd0bd.appspot.com/recipe', {params}).pipe(tap(console.log));
+  }
+
+  getRecipe(r: string) {
+    return this.http.get<RecipeModel>('https://api-dot-mimir-cd0bd.appspot.com/recipe/' + r).pipe(tap(console.log));
   }
 }
