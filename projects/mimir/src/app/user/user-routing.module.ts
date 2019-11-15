451baf9975d7bb66b27b37396d0687ebdc9ee5f1
aalanis <aalanis@uncc.edu>
2019-11-14 19:44:17 -0500

    persona and unfinished favorite

--- projects/mimir/src/app/user/user-routing.module.ts
+++ projects/mimir/src/app/user/user-routing.module.ts
@@ -8,6 +8,7 @@ const routes: Routes = [
       { path: 'home', loadChildren: () => import('../home/home.module').then(m => m.HomeModule ) },
       { path: 'recipe', loadChildren: () => import('../recipe/recipe.module').then(m => m.RecipeModule ) },
       { path: 'person', loadChildren: () => import('../person/person.module').then(m => m.PersonModule)},
+      { path: 'favorite', loadChildren: () => import('../Favorite/favorite.module').then(m => m.FavoriteModule)},
       { path: '', redirectTo: 'home', pathMatch: 'full'}
     ]
   }
