0669d2f96751a4851b30de60698d691829b9f760
LukasGuptaLeary <lukool95@yahoo.com>
2019-11-14 19:54:52 -0500

    Review Page and Auth Guard

--- projects/mimir/src/app/recipe/pages/review/review.component.ts
+++ projects/mimir/src/app/recipe/pages/review/review.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-review',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./review.component.scss']
 })
 export class ReviewComponent implements OnInit {
+  ratings = [];
+  comments = [];
+  date = new Date();
+  timeStamp = '';
+  count = 0;
+
+  addRatingsFormGroup = new FormGroup({
+    rating: new FormControl(null, [])
+  });
+  addCommentsFormGroup = new FormGroup({
+    comment: new FormControl(null, [])
+  });
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  addRating() {
+    const rating = this.addRatingsFormGroup.get('rating');
+    if (rating.value) {
+      this.ratings.pop();
+      this.ratings.push(rating.value);
+      alert('Test');
+    }
+  }
+
+  addCom() {
+    const comment = this.addCommentsFormGroup.get('comment');
+    if (comment.value) {
+      this.comments.push(comment.value);
+      this.timeStamp = formatDate(this.date, 'MM-dd-yyyy @ hh:mm a', 'en-US', 'est');
+      this.count = this.comments.length;
+    }
+  }
 }
