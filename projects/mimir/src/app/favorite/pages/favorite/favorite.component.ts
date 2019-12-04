c38aa387a1d48baba3610688dea8a364c5095492
aalanis <aalanis@uncc.edu>
2019-12-04 13:24:41 -0500

    updarte to main branch

--- projects/mimir/src/app/favorite/pages/favorite/favorite.component.ts
+++ projects/mimir/src/app/favorite/pages/favorite/favorite.component.ts
@@ -5,6 +5,9 @@ import {filter, map} from 'rxjs/operators';
 import {RecipeModel} from '../../../shared/models/recipe.model';
 import {RecipeService} from '../../../shared/recipe.service';
 import {RecipeSearchModel} from '../../../shared/models/recipe-search.model';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-favorite',
@@ -13,15 +16,42 @@ import {RecipeSearchModel} from '../../../shared/models/recipe-search.model';
 })
 export class FavoriteComponent implements OnInit {
 
+  recipeSearch$ = new BehaviorSubject<RecipeSearchModel>(null);
+  recipes$: Observable<RecipeModel[]>;
 
-  recipe = [];
 
-  constructor(private recipeService: RecipeService) { }
+  recipes = [];
+
+
+  addRecipeFormGroup = new FormGroup({
+    recipe: new FormControl(null, [])
+  });
+
+  constructor(private recipeService: RecipeService,
+              private auth: AngularFireAuth,
+              private db: AngularFirestore) { }
 
   ngOnInit() {
 
   }
 
+  addFavorite(recipe: RecipeModel) {
+    const recipe = this.addRecipeFormGroup.get('recipe');
+
+   
+    
+  }
+
+  getRecipeIDFromURI(uri: string) {
+    const uriArray = uri.split('_');
+    return uriArray[uriArray.length - 1];
+  }
+
+  getRecipes() {
+    this.recipeService.recipeSearch().subscribe(recipeSearch => {
+      this.recipeSearch$.next(recipeSearch);
+    });
+  }
 
 
 }
